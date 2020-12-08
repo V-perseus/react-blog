27449749c64a8fd7d5589bdd8b809f9b416edfab
mango-jam-0513 <mangojamstar0513@outlook.com>
2020-12-08 11:31:11 +0300

    update

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -1,16 +1,28 @@
-import React, {useState}from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {FaBars, FaTimes} from 'react-icons/fa';
 import {MdFingerprint} from 'react-icons/md';
+import {Button} from './Button';
 import './Navbar.css';
 
 
 function Navbar() {
 
     const [click, setClick] = useState(false);
+    const [button, setButton] =useState(true);
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false)
 
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    }
+
+    window.addEventListener('resize',showButton);
 
     return (
         <>
